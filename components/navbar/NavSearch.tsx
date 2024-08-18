27a71bdbecffd549cca3a30977a3c24ab40f5b2f
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-19 03:13:42 +0700

    update

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,44 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
 import { Input } from "../ui/input";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 function NavSearch() {
+    const searchParams = useSearchParams();
+    const { replace } = useRouter();
+    const [search, setSearch] = useState(
+        searchParams.get("search")?.toString() || ""
+    );
+
+    const handleSearch = useDebouncedCallback((value: string) => {
+        const params = new URLSearchParams(searchParams);
+        console.log(params);
+        if (value) {
+            params.set("search", value);
+        } else {
+            params.delete("search");
+        }
+        replace(`/products?${params.toString()}`);
+    }, 500);
+
+    useEffect(() => {
+        if (!searchParams.get("search")) {
+            setSearch("");
+        }
+    }, [searchParams.get("search")]);
+
     return (
         <Input
             type='search'
             placeholder='search product...'
             className='max-w-xs dark:bg-muted '
+            onChange={(e) => {
+                setSearch(e.target.value);
+                handleSearch(e.target.value);
+            }}
+            value={search}
         />
     );
 }
