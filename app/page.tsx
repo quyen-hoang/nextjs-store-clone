b195b1f7fd6b87f8310e716fe999f16c1ff377bf
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-19 01:29:50 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import Hero from "@/components/home/Hero";
+
+function HomPage() {
+    return (
+        <>
+            <Hero />
+            <FeaturedProducts />
+        </>
+    );
+}
+export default HomPage;
