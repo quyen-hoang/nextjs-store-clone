5e1051a522afb769d2c592bf901634853ee053d8
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-19 17:53:32 +0700

    update

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,13 +1,24 @@
-import { fetchFavoriteId } from "@/utils/actions";
-import { CardSignInButton } from "../form/Buttons";
-import FavoriteToggleForm from "./FavoriteToggleForm";
-import { auth } from "@clerk/nextjs/server";
-
-async function FavoriteToggleButton({ productId }: { productId: string }) {
-    const { userId } = auth();
-    const favoriteId = await fetchFavoriteId({ productId });
-    if (!userId) return <CardSignInButton />;
-
-    return <FavoriteToggleForm favoriteId={favoriteId} productId={productId} />;
+import { FaHeart } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+function FavoriteToggleButton({ productId }: { productId: string }) {
+    return (
+        <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
+            <FaHeart />
+        </Button>
+    );
 }
 export default FavoriteToggleButton;
+
+// import { fetchFavoriteId } from "@/utils/actions";
+// import { CardSignInButton } from "../form/Buttons";
+// import FavoriteToggleForm from "./FavoriteToggleForm";
+// import { auth } from "@clerk/nextjs/server";
+
+// async function FavoriteToggleButton({ productId }: { productId: string }) {
+//     const { userId } = auth();
+//     const favoriteId = await fetchFavoriteId({ productId });
+//     if (!userId) return <CardSignInButton />;
+
+//     return <FavoriteToggleForm favoriteId={favoriteId} productId={productId} />;
+// }
+// export default FavoriteToggleButton;
