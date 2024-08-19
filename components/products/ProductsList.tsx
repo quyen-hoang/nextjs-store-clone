3fba1aad0f3af3f0437f58ed96445de8ad355128
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-19 15:50:48 +0700

    remove toggle Button

--- components/products/ProductsList.tsx
+++ components/products/ProductsList.tsx
@@ -41,9 +41,9 @@ function ProductsList({ products }: { products: Product[] }) {
                                 </CardContent>
                             </Card>
                         </Link>
-                        <div className='absolute bottom-8 right-8 z-5'>
+                        {/* <div className='absolute bottom-8 right-8 z-5'>
                             <FavoriteToggleButton productId={productId} />
-                        </div>
+                        </div> */}
                     </article>
                 );
             })}
